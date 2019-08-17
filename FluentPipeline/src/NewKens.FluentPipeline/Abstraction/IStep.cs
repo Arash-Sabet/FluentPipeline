a9766909eebddb3299d632dad505c9b4daf17d6f
Arash Sabet <arash.sabet@umplify.com>
2019-08-17 12:20:43 -0400

    Added priliminary abstraction interface and class. #2

---
+++ FluentPipeline/src/NewKens.FluentPipeline/Abstraction/IStep.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace NewKens.FluentPipeline.Abstraction
+{
+    public interface IStep<in TInput, TOutput>
+    {
+        Task<TOutput> ExecuteAsync(TInput input);
+    }
+}
---
+++ FluentPipeline/src/NewKens.FluentPipeline/Abstraction/Pipeline.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NewKens.FluentPipeline.Abstraction
+{
+    public abstract class Pipeline<TInput, TOutput> : IStep<TInput, TOutput>
+    {
+        public Func<TInput, Task<TOutput>> Steps { get; protected set; }
+
+        public Task<TOutput> ExecuteAsync(TInput input)
+            => Steps(input);
+    }
+}
